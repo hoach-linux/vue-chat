ba7d50c54681c6b4a5261c7b3e580bb83c8ef150
hoach-linux <hoachprogrammer@gmail.com>
2022-08-04 19:42:36 +0500

    -v 2

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -3,11 +3,11 @@
     <ion-app>
       <ion-header translucent>
         <ion-toolbar>
-          <ion-title>List</ion-title>
+          <ion-title style="text-align: center;">Chat</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ion-content fullscreen>
+      <ion-content :fullscreen="true">
         <ion-list>
           <ion-list-header> Recent Conversations </ion-list-header>
 
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,28 +2,79 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Music</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+      <ion-list>
+        <ion-item>
+          <ion-thumbnail slot="start">
+            <img src="@/img/dawn-fm-the-weeknd.png" />
+          </ion-thumbnail>
+          <ion-label>
+            <h2>Dawn FM</h2>
+            <p>The Weeknd</p>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-thumbnail slot="start">
+            <img src="@/img/future-nostalgia-dua-lipa.png" />
+          </ion-thumbnail>
+          <ion-label>
+            <h2>Future Nostalgia</h2>
+            <p>Dua Lipa</p>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-thumbnail slot="start">
+            <img src="@/img/montero-lil-nas-x.png" />
+          </ion-thumbnail>
+          <ion-label>
+            <h2>MONTERO</h2>
+            <p>Lil Nas X</p>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-thumbnail slot="start">
+            <img src="@/img/planet-her-doja-cat.png" />
+          </ion-thumbnail>
+          <ion-label>
+            <h2>Planet Her</h2>
+            <p>Doja Cat</p>
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-thumbnail slot="start">
+            <img src="@/img/sour-olivia-rodrigo.png" />
+          </ion-thumbnail>
+          <ion-label>
+            <h2>SOUR</h2>
+            <p>Olivia Rodrigo</p>
+          </ion-label>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
 
 export default defineComponent({
-  name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  name: "Tab2Page",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
 });
 </script>
